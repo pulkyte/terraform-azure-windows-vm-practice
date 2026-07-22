2ac1073a9272cc9203865189b2ef5e50c225445f
pulkyte <pulkit.a.7792@gmail.com>
2026-07-22 13:50:09 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "subscription_id" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "vm_name" {
+  type = string
+}
+
+variable "admin_username" {
+  type = string
+}
+
+variable "admin_password" {
+  type      = string
+  sensitive = true
+}
